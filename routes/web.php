300bc538477ca1ec58b2558bfeebb0a8227604c0
Ansab Naif <ansab20556@gmail.com>
2025-08-21 21:38:27 +0300

    Edit File and Add some routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Controllers\ProgramController;
+use App\Controllers\AuthController;
+use App\Controllers\UserController;
 use App\Core\Router;
 
 $router = new Router();
@@ -8,7 +10,22 @@ $router = new Router();
 $router->get('/oraganization-mvc/public/programs', [ProgramController::class, 'index']);
 $router->get('/oraganization-mvc/public/programs/create', [ProgramController::class, 'create']);
 $router->post('/oraganization-mvc/public/programs', [ProgramController::class, 'store']);
-
 $router->get('/oraganization-mvc/public/programs/{id}/edit', [ProgramController::class, 'edit']);
 $router->post('/oraganization-mvc/public/programs/{id}/update', [ProgramController::class, 'update']);
 $router->post('/oraganization-mvc/public/programs/{id}/delete', [ProgramController::class, 'delete']);
+
+
+$router->post('/oraganization-mvc/public/api/login', [AuthController::class, 'login']);
+
+
+$router->get('/oraganization-mvc/public/users', [UserController::class, 'index']);
+$router->get('/oraganization-mvc/public/users/create', [UserController::class, 'create']);
+$router->post('/oraganization-mvc/public/users', [UserController::class, 'store']);
+$router->get('/oraganization-mvc/public/users/{id}/edit', [UserController::class, 'edit']);
+$router->post('/oraganization-mvc/public/users/{id}/update', [UserController::class, 'update']);
+$router->post('/oraganization-mvc/public/users/{id}/delete', [UserController::class, 'delete']);
+
+
+
+$router->get('/oraganization-mvc/public/api/workfields', [ProgramController::class, 'apiIndex']);
+$router->post('/oraganization-mvc/public/api/workfields/{id}/delete', [ProgramController::class, 'apiDelete']);
