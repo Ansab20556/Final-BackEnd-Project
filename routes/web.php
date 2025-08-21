344101d8a97cbbfe064a7e7d388d44562d8d2f25
Ansab Naif <ansab20556@gmail.com>
2025-08-21 11:55:15 +0300

    Edit this file by adding routes of update and delete

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,12 @@
 use App\Controllers\ProgramController;
 use App\Core\Router;
 
+$router = new Router();
 
-$router=new Router();
-// /oraganization-mvc/public/
+$router->get('/oraganization-mvc/public/programs', [ProgramController::class, 'index']);
+$router->get('/oraganization-mvc/public/programs/create', [ProgramController::class, 'create']);
+$router->post('/oraganization-mvc/public/programs', [ProgramController::class, 'store']);
 
-$router->get('/oraganization-mvc/public/programs',[ProgramController::class,'index']);
-
-$router->get('/oraganization-mvc/public/programs/create',[ProgramController::class,'create']);
-
-$router->post('/oraganization-mvc/public/programs',[ProgramController::class,'store']);
+$router->get('/oraganization-mvc/public/programs/{id}/edit', [ProgramController::class, 'edit']);
+$router->post('/oraganization-mvc/public/programs/{id}/update', [ProgramController::class, 'update']);
+$router->post('/oraganization-mvc/public/programs/{id}/delete', [ProgramController::class, 'delete']);
