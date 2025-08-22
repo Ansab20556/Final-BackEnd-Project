4dbe4c9ced5fb89b7eb83b50dc01488b7893c24c
Ansab Naif <ansab20556@gmail.com>
2025-08-22 14:16:18 +0300

    Add Model of Donation

---
+++ app/models/Donation.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Models;
+
+use App\Core\App;
+
+class Donation
+{
+    function all()
+    {
+        $stm = App::db()->prepare("SELECT * FROM donations ORDER BY created_at DESC");
+        $stm->execute();
+        return $stm->fetchAll();
+    }
+
+    function find($id)
+    {
+        $stm = App::db()->prepare("SELECT * FROM donations WHERE donation_id = :id");
+        $stm->execute(['id' => $id]);
+        return $stm->fetch();
+    }
+
+    function create($donor_name, $donation_type, $amount, $item_description, $donation_date, $confirmed)
+    {
+        $stm = App::db()->prepare("INSERT INTO donations
+            (donor_name, donation_type, amount, item_description, donation_date, confirmed)
+            VALUES(:donor_name, :donation_type, :amount, :item_description, :donation_date, :confirmed)");
+        $stm->execute([
+            'donor_name' => $donor_name,
+            'donation_type' => $donation_type,
+            'amount' => $amount,
+            'item_description' => $item_description,
+            'donation_date' => $donation_date,
+            'confirmed' => $confirmed
+        ]);
+    }
+
+    function update($id, $donor_name, $donation_type, $amount, $item_description, $donation_date, $confirmed)
+    {
+        $stm = App::db()->prepare("UPDATE donations SET
+            donor_name = :donor_name,
+            donation_type = :donation_type,
+            amount = :amount,
+            item_description = :item_description,
+            donation_date = :donation_date,
+            confirmed = :confirmed
+            WHERE donation_id = :id");
+        $stm->execute([
+            'donor_name' => $donor_name,
+            'donation_type' => $donation_type,
+            'amount' => $amount,
+            'item_description' => $item_description,
+            'donation_date' => $donation_date,
+            'confirmed' => $confirmed,
+            'id' => $id
+        ]);
+    }
+
+    function delete($id)
+    {
+        $stm = App::db()->prepare("DELETE FROM donations WHERE donation_id = :id");
+        $stm->execute(['id' => $id]);
+    }
+}
