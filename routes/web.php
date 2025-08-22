90cb80c7a7f77edbc6820ed0c062d8fe5d2a484c
Ansab Naif <ansab20556@gmail.com>
2025-08-22 22:27:24 +0300

    Add API routes of Users

--- routes/web.php
+++ routes/web.php
@@ -9,41 +9,76 @@ use App\Core\Router;
 
 $router = new Router();
 
-$router->get('/oraganization-mvc/public/programs', [ProgramController::class, 'index']);
-$router->get('/oraganization-mvc/public/programs/create', [ProgramController::class, 'create']);
-$router->post('/oraganization-mvc/public/programs', [ProgramController::class, 'store']);
-$router->get('/oraganization-mvc/public/programs/{id}/edit', [ProgramController::class, 'edit']);
-$router->post('/oraganization-mvc/public/programs/{id}/update', [ProgramController::class, 'update']);
-$router->post('/oraganization-mvc/public/programs/{id}/delete', [ProgramController::class, 'delete']);
+/**
+ * ======================
+ * Programs Routes
+ * ======================
+ */
+$router->get('/oraganization-mvc/public/programs', [ProgramController::class, 'index']);       // عرض جميع البرامج
+$router->get('/oraganization-mvc/public/programs/create', [ProgramController::class, 'create']); // فورم إنشاء برنامج
+$router->post('/oraganization-mvc/public/programs', [ProgramController::class, 'store']);       // حفظ برنامج جديد
+$router->get('/oraganization-mvc/public/programs/{id}/edit', [ProgramController::class, 'edit']); // فورم تعديل
+$router->put('/oraganization-mvc/public/programs/{id}', [ProgramController::class, 'update']);   // تحديث البرنامج
+$router->delete('/oraganization-mvc/public/programs/{id}', [ProgramController::class, 'delete']); // حذف برنامج
 
 
+/**
+ * ======================
+ * Users Routes
+ * ======================
+ */
+$router->get('/oraganization-mvc/public/users', [UserController::class, 'index']);       // قائمة المستخدمين
+$router->get('/oraganization-mvc/public/users/create', [UserController::class, 'create']); // فورم إنشاء
+$router->post('/oraganization-mvc/public/users', [UserController::class, 'store']);       // إضافة مستخدم
+$router->get('/oraganization-mvc/public/users/{id}/edit', [UserController::class, 'edit']); // فورم تعديل
+$router->put('/oraganization-mvc/public/users/{id}', [UserController::class, 'update']);   // تحديث مستخدم
+$router->delete('/oraganization-mvc/public/users/{id}', [UserController::class, 'delete']); // حذف مستخدم
+
+
+/**
+ * ======================
+ * Organization Routes
+ * ======================
+ */
+$router->get('/oraganization-mvc/public/organization', [OrganizationController::class, 'index']);       // عرض بيانات المنظمة
+$router->get('/oraganization-mvc/public/organization/create', [OrganizationController::class, 'create']); // فورم إدخال بيانات
+$router->post('/oraganization-mvc/public/organization', [OrganizationController::class, 'store']);       // تخزين بيانات
+$router->get('/oraganization-mvc/public/organization/{id}/edit', [OrganizationController::class, 'edit']); // تعديل
+$router->put('/oraganization-mvc/public/organization/{id}', [OrganizationController::class, 'update']);   // تحديث
+$router->delete('/oraganization-mvc/public/organization/{id}', [OrganizationController::class, 'delete']); // حذف
+
+
+/**
+ * ======================
+ * Donations Routes
+ * ======================
+ */
+$router->get('/oraganization-mvc/public/donations', [DonationController::class, 'index']);       // قائمة التبرعات
+$router->get('/oraganization-mvc/public/donations/create', [DonationController::class, 'create']); // فورم إنشاء
+$router->post('/oraganization-mvc/public/donations', [DonationController::class, 'store']);       // إضافة تبرع
+$router->get('/oraganization-mvc/public/donations/{id}/edit', [DonationController::class, 'edit']); // تعديل
+$router->put('/oraganization-mvc/public/donations/{id}', [DonationController::class, 'update']);   // تحديث (صح هنا بدون /update)
+$router->delete('/oraganization-mvc/public/donations/{id}', [DonationController::class, 'delete']);
+
+/**
+ * ======================
+ * Auth / API Routes
+ * ======================
+ */
 $router->post('/oraganization-mvc/public/api/login', [AuthController::class, 'login']);
 
-
-$router->get('/oraganization-mvc/public/users', [UserController::class, 'index']);
-$router->get('/oraganization-mvc/public/users/create', [UserController::class, 'create']);
-$router->post('/oraganization-mvc/public/users', [UserController::class, 'store']);
-$router->get('/oraganization-mvc/public/users/{id}/edit', [UserController::class, 'edit']);
-$router->post('/oraganization-mvc/public/users/{id}/update', [UserController::class, 'update']);
-$router->post('/oraganization-mvc/public/users/{id}/delete', [UserController::class, 'delete']);
-
-
-
+// Workfields API
 $router->get('/oraganization-mvc/public/api/workfields', [ProgramController::class, 'apiIndex']);
-$router->post('/oraganization-mvc/public/api/workfields/{id}/delete', [ProgramController::class, 'apiDelete']);
+$router->delete('/oraganization-mvc/public/api/workfields/{id}', [ProgramController::class, 'apiDelete']);
 
+// Uers API
+// API routes
+$router->get('/oraganization-mvc/public/api/v1/users', [UserController::class, 'apiIndex']);
+$router->get('/oraganization-mvc/public/api/v1/users/{id}', [UserController::class, 'apiShow']);
+$router->post('/oraganization-mvc/public/api/v1/users', [UserController::class, 'apiStore']);
+$router->delete('/oraganization-mvc/public/api/v1/users/{id}', [UserController::class, 'apiDelete']);
 
-$router->get('/oraganization-mvc/public/organization', [OrganizationController::class, 'index']);
-$router->get('/oraganization-mvc/public/organization/create', [OrganizationController::class, 'create']);
-$router->post('/oraganization-mvc/public/organization', [OrganizationController::class, 'store']);
-$router->get('/oraganization-mvc/public/organization/{id}/edit', [OrganizationController::class, 'edit']);
-$router->post('/oraganization-mvc/public/organization/{id}/update', [OrganizationController::class, 'update']);
-$router->post('/oraganization-mvc/public/organization/{id}/delete', [OrganizationController::class, 'delete']);
-
-
-$router->get('/oraganization-mvc/public/donations', [DonationController::class, 'index']);
-$router->get('/oraganization-mvc/public/donations/create', [DonationController::class, 'create']);
-$router->post('/oraganization-mvc/public/donations', [DonationController::class, 'store']);
-$router->get('/oraganization-mvc/public/donations/{id}/edit', [DonationController::class, 'edit']);
-$router->post('/oraganization-mvc/public/donations/{id}/update', [DonationController::class, 'update']);
-$router->post('/oraganization-mvc/public/donations/{id}/delete', [DonationController::class, 'delete']);
+// Donations API
+$router->get('/api/v1/donations', [DonationController::class, 'apiIndex']);
+$router->post('/api/v1/donations', [DonationController::class, 'apiStore']);
+$router->delete('/api/v1/donations/{id}', [DonationController::class, 'apiDelete']);
