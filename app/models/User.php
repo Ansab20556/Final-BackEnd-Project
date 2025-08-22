9e25023bd1773a618940464f10c7292804bae733
Ansab Naif <ansab20556@gmail.com>
2025-08-22 11:30:09 +0300

    Add some styles

--- app/models/User.php
+++ app/models/User.php
@@ -3,27 +3,32 @@ namespace App\Models;
 
 use App\Core\App;
 
-class User {
+class User 
+{
 
-    public function findByEmail($email) {
+    public function findByEmail($email) 
+    {
         $stmt = App::db()->prepare("SELECT * FROM users WHERE email = ?");
         $stmt->execute([$email]);
         return $stmt->fetch();
     }
 
-    function all() {
+    function all() 
+    {
         $stm = App::db()->prepare("SELECT * FROM users");
         $stm->execute();
         return $stm->fetchAll();
     }
 
-    function find($id) {
+    function find($id) 
+    {
         $stm = App::db()->prepare("SELECT * FROM users WHERE id = :id");
         $stm->execute(['id' => $id]);
         return $stm->fetch();
     }
 
-    function create($username, $email, $password, $role) {
+    function create($username, $email, $password, $role) 
+    {
         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
         $stm = App::db()->prepare("INSERT INTO users(username, email, password, role) VALUES(:username, :email, :password, :role)");
         $stm->execute([
@@ -34,7 +39,8 @@ class User {
         ]);
     }
 
-    function update($id, $username, $email, $password, $role) {
+    function update($id, $username, $email, $password, $role) 
+    {
         $stm = App::db()->prepare("UPDATE users SET username = :username, email = :email, password = :password, role = :role WHERE id = :id");
         $stm->execute([
             'username' => $username,
@@ -45,7 +51,8 @@ class User {
         ]);
     }
 
-    function delete($id) {
+    function delete($id) 
+    {
         $stm = App::db()->prepare("DELETE FROM users WHERE id = :id");
         $stm->execute(['id' => $id]);
     }
