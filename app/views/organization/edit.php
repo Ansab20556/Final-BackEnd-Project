d5f897ccc81ff6cf5fd161ce72fb7be4f3955a4a
Ansab Naif <ansab20556@gmail.com>
2025-08-22 22:22:52 +0300

    Edit some styles of organization data

--- app/views/organization/edit.php
+++ app/views/organization/edit.php
@@ -6,20 +6,22 @@
         <script src="https://cdn.tailwindcss.com"></script>
     </head>
     <body class="bg-gray-100">
-        <div class="max-w-xl mx-auto p-6 bg-white rounded shadow mt-10">
-            <h1 class="text-2xl font-bold mb-6">تعديل بيانات المنظمة</h1>
+            <div class="max-w-xl mx-auto p-6 bg-white rounded shadow mt-10">
+                <h1 class="text-2xl font-bold mb-6">تعديل بيانات المنظمة</h1>
 
-            <form method="POST" action="/oraganization-mvc/public/organization/<?= $organization['id'] ?>/update">
-                <input type="text" name="name" value="<?= htmlspecialchars($organization['name']) ?>" class="w-full border p-2 mb-3">
-                <input type="text" name="logo" value="<?= htmlspecialchars($organization['logo']) ?>" class="w-full border p-2 mb-3">
-                <textarea name="vision" class="w-full border p-2 mb-3"><?= htmlspecialchars($organization['vision']) ?></textarea>
-                <textarea name="mission" class="w-full border p-2 mb-3"><?= htmlspecialchars($organization['mission']) ?></textarea>
-                <textarea name="goals[]" class="w-full border p-2 mb-3"><?= htmlspecialchars($organization['goals']) ?></textarea>
+                <form method="POST" action="/oraganization-mvc/public/organization/<?= $organization['id'] ?>">
+                    <input type="hidden" name="_method" value="PUT">
 
-                <button type="submit" class="px-4 py-2 bg-amber-500 text-white rounded">تحديث</button>
-                <a href="/oraganization-mvc/public/organization"
-                class="px-4 py-2 bg-gray-300 rounded">إلغاء</a>
-            </form>
-        </div>
+                    <input type="text" name="name" value="<?= htmlspecialchars($organization['name']) ?>" class="w-full border p-2 mb-3">
+                    <input type="text" name="logo" value="<?= htmlspecialchars($organization['logo']) ?>" class="w-full border p-2 mb-3">
+                    <textarea name="vision" class="w-full border p-2 mb-3"><?= htmlspecialchars($organization['vision']) ?></textarea>
+                    <textarea name="mission" class="w-full border p-2 mb-3"><?= htmlspecialchars($organization['mission']) ?></textarea>
+                    <textarea name="goals[]" class="w-full border p-2 mb-3"><?= htmlspecialchars($organization['goals']) ?></textarea>
+
+                    <button type="submit" class="px-4 py-2 bg-amber-500 text-white rounded">تحديث</button>
+                    <a href="/oraganization-mvc/public/organization"
+                    class="px-4 py-2 bg-gray-300 rounded">إلغاء</a>
+                </form>
+            </div>
     </body>
 </html>
