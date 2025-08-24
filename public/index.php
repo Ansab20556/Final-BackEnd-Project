9b1c4f1f23fdd47ba365c98258528a1cb9e53491
Ansab Naif <ansab20556@gmail.com>
2025-08-24 08:55:33 +0300

    Add Access Control Allow Origin

--- public/index.php
+++ public/index.php
@@ -1,4 +1,15 @@
 <?php
+// index.php (أول الملف)
+header("Access-Control-Allow-Origin: http://localhost:5173"); // أو * للتجربة
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Content-Type: application/json; charset=UTF-8");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') 
+    {
+        http_response_code(200);
+        exit;
+    }
 
 // Simple PSR-4-ish autoloader for App\*
 spl_autoload_register(function ($class) {
