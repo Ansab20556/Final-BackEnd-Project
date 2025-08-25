f0fea345388f5301f7544ea6a88be16cb6cb9c40
Ansab Naif <ansab20556@gmail.com>
2025-08-25 20:44:31 +0300

    Edit code and Apply PSR-1 & PSR-12

--- app/core/FileLogger.php
+++ app/core/FileLogger.php
@@ -1,12 +1,22 @@
 <?php
+
 namespace App\Core;
 
-class FileLogger implements Logger {
-    public function log(string $message): void {
+class FileLogger implements Logger
+{
+    /**
+     * تسجيل رسالة في ملف app.log
+     *
+     * @param string $message الرسالة المراد تسجيلها
+     */
+    public function log(string $message): void
+    {
         $dir = __DIR__ . '/../../logs';
+
         if (!is_dir($dir)) {
             mkdir($dir, 0777, true);
         }
-        file_put_contents($dir . "/app.log", $message . PHP_EOL, FILE_APPEND);
+
+        file_put_contents($dir . '/app.log', $message . PHP_EOL, FILE_APPEND);
     }
 }
