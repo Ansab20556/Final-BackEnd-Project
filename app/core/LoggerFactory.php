aa9ea377cf968d6f60e322a72caab4a5107c5b8e
Ansab Naif <ansab20556@gmail.com>
2025-08-25 20:45:21 +0300

    Edit code and Apply PSR-1 & PSR-12

--- app/core/LoggerFactory.php
+++ app/core/LoggerFactory.php
@@ -1,10 +1,22 @@
 <?php
+
 namespace App\Core;
 
-class LoggerFactory {
-    public static function create(string $type): Logger {
-        return match($type) {
-            "file" => new FileLogger(),
+/**
+ * Factory لإنشاء كائنات Logger مختلفة
+ */
+class LoggerFactory
+{
+    /**
+     * إنشاء Logger حسب النوع
+     *
+     * @param string $type نوع الـ Logger (مثال: 'file')
+     * @return Logger
+     */
+    public static function create(string $type): Logger
+    {
+        return match ($type) {
+            'file' => new FileLogger(),
             // لاحقاً يمكن إضافة DatabaseLogger أو أي Logger آخر
         };
     }
